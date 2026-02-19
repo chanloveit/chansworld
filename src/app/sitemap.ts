2fc4ad2f773dbfeb1c9c7fa0db24f7beea72794b
chanloveit <ch20041001@gmail.com>
2026-02-19 13:00:04 +0000

    feat: add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap{
 	const posts = getAllPosts();
 	const baseUrl = 'https://chansworld.vercel.app';
 	
-	const postUrls = posts.map((post) = ({
+	const postUrls = posts.map((post) => ({
 		url: `${baseUrl}/post${post.id}`,
 		lastModified: new Date(post.created_at)
 	}));
