2dd848f98ea5505b4d0cad2543ac8b7020b6a657
chanloveit <ch20041001@gmail.com>
2026-02-19 08:11:18 +0000

    feat: markdown render

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,3 +1,21 @@
-export default function Page(){
+import { getAllPosts, getPostById } from '@/utils/posts';
+import { notFound } from 'next/navigation';
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import CategoryTag from '@/components/CategoryTag';
+import MarkdownRenderer from '@/components/MarkdownRenderer';
+
+export default async function PostPage({ params }: Promise<{ id: string }>){
+	const { id } = await params;
+	const post = getPostById(Number(id));
+
+	if(!post){
+		notFound();
+	}
 	
+	return(
+		<article className = 'prose'>
+			<MarkdownRenderer content = {post.content} />
+		</article>
+	)
 }
---
+++ src/components/MarkdownRenderer.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import LiteYoutubeEmbed from 'react-lite-youtube-embed';
+
+export default function MarkdownRenderer({ content }: { content: string}){
+	return(
+		<ReactMarkdown
+			remarkPlugins = {[remarkGfm]}
+			components = {{
+				p({ children, ...props}: any){
+					const text = String(children);
+					const match = /^youtube:([a-zA-Z0-9_-]+)$/.exec(text.trim());
+					if(match){
+						return <LiteYoutubeEmbed id = {match[1]} title = 'youtube' />;
+					}
+
+					else{
+						<p {...props}>{children}</p>
+					}
+				},
+				
+				code({ node, className, children, ...props }: any){
+					const match = /language-(\w+)/.exec(className || '');
+					const inline = !match;
+
+					return !inline ? (
+						<SyntaxHighlighter
+							style = {vscDarkPlus}
+							language = {match[1]}
+							PreTag = 'div'
+							customStyle = {{
+								background: 'var(--color-bg-2)'
+							}}
+							codeTagProps = {{ style: { fontFamily: 'var(--font-jetbrains)' } }}
+							{...props}>
+							{String(children).replace(/\n$/, '')}
+						</SyntaxHighlighter>
+					) : (
+						<code className = '{className} {...props}'>{children}</code>
+					);
+				}
+			}}>{content}</ReactMarkdown>
+	)
+}
